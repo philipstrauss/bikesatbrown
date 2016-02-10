cbf970e5f35e707cb525b8fb05af3fd8d7c76992
philipstrauss <philip_strauss@brown.edu>
2016-02-09 22:12:45 -0500

    Announcements

--- bikesatbrown.js
+++ bikesatbrown.js
@@ -4,9 +4,13 @@ bikeCollection = new Mongo.Collection("bikeCollection");
 if (Meteor.isClient) {
 
   Template.body.helpers({
-    availableBikes: function () {
+    availableBikesString: function () {
       if (bikeCollection.find({"name":"count"}).count() > 0){
-        return bikeCollection.findOne({"name":"count"}).val;
+
+        if (bikeCollection.findOne({"name":"count"}).val == 1 )
+          return "There is currently 1 bike available."
+
+        return "There are currently " + String(bikeCollection.findOne({"name":"count"}).val) + " bikes available.";
       }
     },
     isOpen: function () {
@@ -33,6 +37,13 @@ if (Meteor.isClient) {
           return "color:red";
         }
       }
+    },
+    announcement: function () {
+      if (bikeCollection.find({"name":"announcement"}).count() > 0){
+
+        return bikeCollection.findOne({"name":"announcement"}).val;
+
+      }
     }
   });
 
@@ -47,24 +58,40 @@ if (Meteor.isClient) {
 
       var countID = bikeCollection.findOne({"name":"count"})._id;
       var openID = bikeCollection.findOne({"name":"open"})._id;
+      var announcementID = bikeCollection.findOne({"name":"announcement"})._id;
 
-      if(text==="increase"){
+      if(text === "increase"){
 
         bikeCollection.update({"_id":countID}, {$inc: {val: 1}});
       }
-      else if (text==="decrease"){
+      else if (text === "decrease"){
 
         if( bikeCollection.findOne({"_id":countID}).val > 0 ){
           bikeCollection.update({"_id":countID}, {$inc: {val: -1}});
         }
       }
-      else if (text==="open"){
+      else if (text === "open"){
 
         bikeCollection.update({"_id":openID}, {$set: {val: true}});
       }
-      else if (text==="close"){
+      else if (text === "close"){
 
         bikeCollection.update({"_id":openID}, {$set: {val: false}});
+      }
+      else if (text.slice(0,3) === "set"){
+
+        if ( text.split(" ").length == 2 ){
+          if ( !isNaN( parseInt( text.split(" ")[1] ) ) ){
+
+            var newVal = parseInt( text.split(" ")[1] );
+            if (newVal >= 0)
+              bikeCollection.update({"_id":countID}, {$set: {val: newVal}});
+          }
+        }
+      }
+      else if (text.split(" ")[0] == "announce"){
+
+        bikeCollection.update({"_id":announcementID}, {$set: {val: text.slice(8)}});
       }
  
       // Clear form
@@ -77,5 +104,6 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
       bikeCollection.insert({"name":"count", "val":0});
       bikeCollection.insert({"name":"open", "val":false});
+      bikeCollection.insert({"name":"announcement", "val":""});
   });
 }
